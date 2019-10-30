bd4beba12f955ead61b4de29512838fc0981575f
Ted Michael Celis <krazete@gmail.com>
2019-10-30 16:39:26 -0700

    game is mostly playable now

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
+/* Elements */
+
 var board;
-var empty = [];
 var tiles = [];
+
+/* Data */
+
+var empty = [];
+var ids = [];
 var selected;
 
 function newTile(n) {
     var tile = document.createElement("div");
+    tile.id = n;
     tile.className = "tile";
     var icon = document.createElement("img");
-    icon.src = "tile/" + n + ".png";
+    icon.src = "tile/" + (n % 18 + 1) + ".png";
     tile.appendChild(icon);
     return tile;
 }
@@ -15,10 +22,8 @@ function newTile(n) {
 function initTiles() {
     for (var k = 0; k < 36; k++) {
         empty.push(false);
-        tiles.push({
-            "id": k,
-            "element": newTile(k % 18 + 1)
-        });
+        ids.push(k);
+        tiles.push(newTile(k));
     }
 }
 
@@ -32,13 +37,27 @@ function shuffle() {
     for (var l = ks.length - 1; l > 0; l--) {
         var m = ks[l];
         var n = ks[Math.floor(Math.random() * (l + 1))];
-        var tilesm = tiles[m];
-        tiles[m] = tiles[n];
-        tiles[n] = tilesm;
+        var idsm = ids[m];
+        ids[m] = ids[n];
+        ids[n] = idsm;
     }
 }
 
-function hscan(x, y) {
+function updateBoard() {
+    board.innerHTML = "";
+    for (var k = 0; k < 36; k++) {
+        var tile = tiles[ids[k]];
+        board.appendChild(tile);
+        if (empty[k]) {
+            tile.classList.add("hidden");
+        }
+        else {
+            tile.classList.remove("hidden");
+        }
+    }
+}
+
+function getDomain(x, y) {
     var domain = [-1, 9];
     for (var i = x - 1; i > -1; i--) {
         if (!empty[i + 9 * y]) {
@@ -55,7 +74,7 @@ function hscan(x, y) {
     return domain;
 }
 
-function vscan(x, y) {
+function getRange(x, y) {
     var range = [-1, 4];
     for (var j = y - 1; j > -1; j--) {
         if (!empty[x + 9 * j]) {
@@ -72,39 +91,41 @@ function vscan(x, y) {
     return range;
 }
 
-function trace(p, q) {
+function solve(p, q) {
     var solutions = [];
 
     var px = p % 9;
     var py = Math.floor(p / 9);
     var qx = q % 9;
     var qy = Math.floor(q / 9);
-    var dx = qx - px < 0 ? -1 : 1;
-    var dy = qy - py < 0 ? -1 : 1;
 
-    var ph = hscan(px, py);
-    var qh = hscan(qx, qy);
-    for (var i = qh[0]; dx > 0 ? i <= ph[1] : i >= ph[1]; i += dx) {
-        for (var j = py; dy > 0 ? j <= qy : j >= qy; j += dy) {
+    var pd = getDomain(px, py);
+    var qd = getDomain(qx, qy);
+    var domain = [Math.max(pd[0], qd[0]), Math.min(pd[1], qd[1])];
+    for (var i = domain[0]; i <= domain[1]; i++) {
+        var interrupted = false;
+        for (var j = Math.min(py, qy) + 1; j < Math.max(py, qy); j++) {
             var k = i + 9 * j;
-            if (!(i < 0 || i >= 9 || j < 0 || j >= 4 || empty[k] || k == p || k == q)) {
+            if (!(i < 0 || i > 8 || j < 0 || j > 3 || empty[k])) {
                 break;
             }
         }
-        if (j - dy == qy) {
+        if (j == py || j == qy) {
             solutions.push([[px, py], [i, py], [i, qy], [qx, qy]]);
         }
     }
-    var pv = vscan(px, py);
-    var qv = vscan(qx, qy);
-    for (var j = qv[0]; dy > 0 ? j <= pv[1] : j >= pv[1]; j += dy) {
-        for (var i = px; dx > 0 ? i <= qx : i >= qx; i += dx) {
+    var pr = getRange(px, py);
+    var qr = getRange(qx, qy);
+    var range = [Math.max(pr[0], qr[0]), Math.min(pr[1], qr[1])];
+    for (var j = range[0]; j <= range[1]; j++) {
+        var interrupted = false;
+        for (var i = Math.min(px, qx) + 1; i < Math.max(px, qx); i++) {
             var k = i + 9 * j;
-            if (!(i < 0 || i >= 9 || j < 0 || j >= 4 || empty[k] || k == p || k == q)) {
+            if (!(i < 0 || i > 8 || j < 0 || j > 3 || empty[k])) {
                 break;
             }
         }
-        if (i - dx == qx) {
+        if (i == px || i == qx) {
             solutions.push([[px, py], [px, j], [qx, j], [qx, qy]]);
         }
     }
@@ -114,56 +135,69 @@ function trace(p, q) {
         empty[q] = true;
     }
 
-    return solutions[Math.floor(Math.random() * solutions.length)];
-}
-
-function match(p, q) {
-    if (tiles[p] % 18 == tiles[q] % 18) {
-        var solution = trace(p, q);
-    }
-}
-
-function updateBoard() {
-    board.innerHTML = "";
-    for (var k = 0; k < 36; k++) {
-        var tile = tiles[k];
-        board.appendChild(tile.element);
-        if (empty[k]) {
-            tile.element.classList.add("hidden");
-        }
-        else {
-            tile.element.classList.remove("hidden");
-        }
-    }
+    return solutions;
 }
 
 var linelayer;
 
-function layline() {
-    for (var polyline of linelayer.children) {
-        polyline.setAttribute("points", "50,300 500,300 500,50 900,50");
-    }
+// function getCentroid(tile) {
+//     var box = tile.getBoundingClientRect();
+//     return [(box.left + box.right) / 2, (box.top + box.bottom) / 2];
+// }
+
+// function layline(ks) {
+//     for (var polyline of linelayer.children) {
+//         console.log(ks);
+//         polyline.setAttribute("points", [
+//             getCentroid(tiles[ks[0]]),
+//             getCentroid(tiles[ks[1]]),
+//             getCentroid(tiles[ks[2]]),
+//             getCentroid(tiles[ks[3]])
+//         ].join(" "));
+//     }
+// }
+
+function animateMatch(p, q) {
+    updateBoard();
 }
 
-function animateMatch() {
-    match(selected, tile);
+function match(selected, id) {
+    if (selected % 18 == id % 18 && selected != id) {
+        var p = ids.indexOf(selected);
+        var q = ids.indexOf(id);
+        var solutions = solve(p, q);
+        if (solutions.length) {
+            // var solution = solutions[Math.floor(Math.random() * solutions.length)];
+            for (var i = 0; i < solutions.length; i++) {
+                var solution = solutions[i];
+                console.log(solution);
+            }
+            return true;
+        }
+        return false;
+    }
+    return false;
 }
 
 function select(tile) {
+    if (tile.target) {
+        tile = tile.target;
+    }
+    var id = parseInt(tile.id);
     if (tile.classList.contains("tile")) {
         if (selected) {
-            if (selected == tile) {
-                tile.classList.remove("selected");
+            if (match(selected, id)) {
+                tile.classList.add("selected");
+                animateMatch(selected, id); // maybe put in match()
             }
             else {
-                tile.classList.add("selected");
-                animateMatch(selected, tile);
+                tiles[selected].classList.remove("selected");
             }
             selected = undefined;
         }
         else {
             tile.classList.add("selected");
-            selected = tile;
+            selected = id;
         }
     }
     else if (tile.id != "board") {
@@ -171,28 +205,13 @@ function select(tile) {
     }
 }
 
-function onBoardClick(e) {
-    select(e.target);
-}
-
 function init() {
     linelayer = document.getElementById("linelayer");
     board = document.getElementById("board");
-    board.addEventListener("click", onBoardClick);
+    board.addEventListener("click", select);
     initTiles();
-    test();
-}
-
-window.addEventListener("DOMContentLoaded", init);
-
-function test() {
-    for (var i = 1; i < 8; i++) {
-        empty[i] = true;
-    }
-    for (var i = 0; i < 3; i++) {
-        empty[4 + i * 9] = true;
-    }
-    match(7 + 9 * 1, 4 + 9 * 3);
     shuffle();
     updateBoard();
 }
+
+window.addEventListener("DOMContentLoaded", init);
