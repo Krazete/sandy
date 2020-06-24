9673eb3876d83c0a70a23bd0a6e455230f8fd655
Ted Michael Celis <krazete@gmail.com>
2020-06-23 17:13:44 -0700

    in the middle of objectifying board logic
    everything's broken rn

--- index.js
+++ index.js
@@ -18,25 +18,9 @@ var ids = [];
 var pid;
 var matches = 0;
 
+var w, h, n;
+
 /* Initialize Board */
-
-function newTile(id) {
-    var tile = document.createElement("div");
-    var icon = document.createElement("img");
-    tile.id = id;
-    tile.className = "tile";
-    icon.src = "tile/" + (id % 18 + 1) + ".png";
-    tile.appendChild(icon);
-    return tile;
-}
-
-function initTiles() {
-    for (var id = 0; id < 36; id++) {
-        empty.push(false);
-        ids.push(id);
-        tiles.push(newTile(id));
-    }
-}
 
 function updateBoard() {
     for (var i = 0; i < 36; i++) {
@@ -59,140 +43,6 @@ function disableShuffle() {
 
 function enableShuffle() {
     shuffle.classList.remove("disabled");
-}
-
-function shuffleTiles() {
-    var nonempty = []
-    for (var i = 0; i < 36; i++) {
-        if (!empty[i]) {
-            nonempty.push(i);
-        }
-    }
-    for (var i = nonempty.length - 1; i > 0; i--) {
-        var a = nonempty[i];
-        var b = nonempty[Math.floor(Math.random() * (i + 1))];
-        var idsa = ids[a];
-        ids[a] = ids[b];
-        ids[b] = idsa;
-    }
-    updateBoard();
-    disableShuffle();
-    setTimeout(enableShuffle, 3000);
-}
-
-/* Path Finding */
-
-function getDomain(x, y) {
-    var domain = [-1, 9];
-    for (var i = x - 1; i > -1; i--) {
-        if (!empty[i + 9 * y]) {
-            domain[0] = i + 1;
-            break;
-        }
-    }
-    for (var i = x + 1; i < 9; i++) {
-        if (!empty[i + 9 * y]) {
-            domain[1] = i - 1;
-            break;
-        }
-    }
-    return domain;
-}
-
-function getRange(x, y) {
-    var range = [-1, 4];
-    for (var j = y - 1; j > -1; j--) {
-        if (!empty[x + 9 * j]) {
-            range[0] = j + 1;
-            break;
-        }
-    }
-    for (var j = y + 1; j < 4; j++) {
-        if (!empty[x + 9 * j]) {
-            range[1] = j - 1;
-            break;
-        }
-    }
-    return range;
-}
-
-function findPaths(p, q) {
-    if (ids[p] == ids[q] || ids[p] % 18 != ids[q] % 18) { /* invalid match */
-        return [];
-    }
-
-    var paths = [];
-
-    var px = p % 9;
-    var py = Math.floor(p / 9);
-    var qx = q % 9;
-    var qy = Math.floor(q / 9);
-
-    var pd = getDomain(px, py);
-    var qd = getDomain(qx, qy);
-    var domain = [Math.max(pd[0], qd[0]), Math.min(pd[1], qd[1])];
-
-    var pr = getRange(px, py);
-    var qr = getRange(qx, qy);
-    var range = [Math.max(pr[0], qr[0]), Math.min(pr[1], qr[1])];
-
-    for (var i = domain[0]; i <= domain[1]; i++) {
-        for (var j = Math.min(py, qy) + 1; j < Math.max(py, qy); j++) {
-            var k = i + 9 * j;
-            if (!(i < 0 || i > 8 || j < 0 || j > 3 || empty[k])) {
-                break;
-            }
-        }
-        if (j == py || j == qy) {
-            paths.push([[px, py], [i, py], [i, qy], [qx, qy]]);
-        }
-    }
-    for (var j = range[0]; j <= range[1]; j++) {
-        for (var i = Math.min(px, qx) + 1; i < Math.max(px, qx); i++) {
-            var k = i + 9 * j;
-            if (!(i < 0 || i > 8 || j < 0 || j > 3 || empty[k])) {
-                break;
-            }
-        }
-        if (i == px || i == qx) {
-            paths.push([[px, py], [px, j], [qx, j], [qx, qy]]);
-        }
-    }
-    return paths;
-}
-
-/* Path Trimming */
-
-function lineLength(p, q) {
-    /* p and q form a non-diagonal line */
-    return Math.abs(q[0] - p[0]) + Math.abs(q[1] - p[1]);
-}
-
-function pathLength(path) {
-    var a = lineLength(path[0], path[1]);
-    var b = lineLength(path[1], path[2]);
-    var c = lineLength(path[2], path[3]);
-    if (a == 0 || b == 0 || c == 0) { /* prefer fewer corners */
-        return 0;
-    }
-    return a + b + c;
-}
-
-function byLength(path1, path2) {
-    return pathLength(path1) - pathLength(path2);
-}
-
-function trimPaths(paths) { /* mutating function */
-    paths.sort(byLength);
-    var minlength = pathLength(paths[0]);
-    for (var i = paths.length - 1; i >= 0; i--) {
-        if (pathLength(paths[i]) > minlength) {
-            paths.pop();
-        }
-        else {
-            break;
-        }
-    }
 }
 
 /* Draw Path */
@@ -280,6 +130,8 @@ function select(tile) {
 
 /* Start Game */
 
+var currentBoard;
+
 function startGame() {
     game.classList.remove("idle");
     banner.classList.add("hidden");
@@ -290,8 +142,9 @@ function startGame() {
     }
     setTimeout(reallyStart, 4000);
 
-    initTiles();
-    shuffleTiles();
+    // currentBoard = new Board("tile", 18);
+    currentBoard = new Board("tile", 18);
+    currentBoard.shuffle();
 }
 
 function reallyStart() {
@@ -303,6 +156,10 @@ function reallyStart() {
 function endGame() {
     game.classList.add("idle");
     result.classList.remove("hidden");
+}
+
+function shuffleTiles() {
+    currentBoard.shuffle();
 }
 
 /* Restart Game */
---
+++ x.js
@@ -0,0 +1,197 @@
+var boards = {};
+
+function getDimensions(n) { /* get nonlinear factors of 2n with a ratio wider than 16:9 */
+    var i, j, w, h;
+    for (i = 2; i < 2 * n; i++) {
+        j = 2 * n / i;
+        if (16 * i >= 9 * j) {
+            break;
+        }
+        if (j == Math.floor(j)) {
+            w = j;
+            h = i;
+        }
+    }
+    return [w, h];
+}
+
+function Board(root, n) {
+    if (root in boards) {
+        return boards[root];
+    }
+
+    var dimensions = getDimensions(n);
+
+    if (typeof dimensions[0] == "undefined" || typeof dimensions[1] == "undefined") {
+        return;
+    }
+
+    this.length = n;
+    this.width = dimensions[0];
+    this.height = dimensions[1];
+    this.tileset = [];
+    this.activeset = [];
+    this.orderset = [];
+
+    for (var i = 0; i < n; i++) {
+        for (var j = 0; j < 2; j++) {
+            var icon = new Image();
+            icon.src = root + "/" + (i + 1) + ".png";
+
+            var tile = document.createElement("div");
+            tile.appendChild(icon);
+
+            this.tileset.push(tile);
+            this.activeset.push(true);
+            this.orderset.push(2 * j + i);
+        }
+    }
+
+    boards[root] = this;
+
+    return this;
+}
+
+Board.prototype.shuffle = function () {
+    var iset = [];
+
+    for (var i = 0; i < this.activeset.length; i++) {
+        if (this.activeset[i]) {
+            iset.push(i);
+        }
+    }
+
+    for (var i = iset.length - 1; i > 0; i--) {
+        var a = iset[i];
+        var b = iset[Math.floor(Math.random() * (i + 1))];
+        var orderseta = this.orderset[a];
+        this.orderset[a] = this.orderset[b];
+        this.orderset[b] = orderseta;
+    }
+};
+
+Board.prototype.getDomain = function (x, y) {
+    var domain = [-1, this.width];
+
+    for (var i = x - 1; i > -1; i--) {
+        if (this.activeset[i + this.width * y]) {
+            domain[0] = i + 1;
+            break;
+        }
+    }
+
+    for (var i = x + 1; i < this.width; i++) {
+        if (this.activeset[i + this.width * y]) {
+            domain[1] = i - 1;
+            break;
+        }
+    }
+
+    return domain;
+}
+
+Board.prototype.getRange = function (x, y) {
+    var range = [-1, this.height];
+    for (var j = y - 1; j > -1; j--) {
+        if (this.activeset[x + this.width * j]) {
+            range[0] = j + 1;
+            break;
+        }
+    }
+
+    for (var j = y + 1; j < this.height; j++) {
+        if (this.activeset[x + this.width * j]) {
+            range[1] = j - 1;
+            break;
+        }
+    }
+
+    return range;
+}
+
+function manhattanDistance(a, b) {
+    return Math.abs(b[0] - a[0]) + Math.abs(b[1] - a[1]);
+}
+
+function pathLength(path) {
+    var ab = manhattanDistance(path[0], path[1]);
+    var bc = manhattanDistance(path[1], path[2]);
+    var cd = manhattanDistance(path[2], path[3]);
+
+    if (ab == 0 || bc == 0 || cd == 0) { /* prefer fewer segments */
+        return 0;
+    }
+
+    return ab + bc + cd;
+}
+
+function byLength(path1, path2) {
+    return pathLength(path1) - pathLength(path2);
+}
+
+function shortestPaths(paths) {
+    var shorts = [];
+    var minlength;
+
+    for (var i = 0; i < paths.length; i++) {
+        var ilength = pathLength(paths[i]);
+        if (typeof minlength == "undefined" || ilength < minlength) {
+            minlength = ilength;
+            shorts = [paths[i]];
+        }
+        else {
+            shorts.push(paths[i]);
+        }
+    }
+
+    return shorts;
+}
+
+Board.prototype.findPaths = function (p, q) {
+    var paths = [];
+
+    if (
+        this.orderset[p] != this.orderset[q] &&
+        this.orderset[p] % this.length == this.orderset[q] % this.length
+    ) {
+        var px = p % this.width;
+        var py = Math.floor(p / this.width);
+        var qx = q % this.width;
+        var qy = Math.floor(q / this.width);
+        console.log(px, py, qx, qy);
+
+        var pd = this.getDomain(px, py);
+        var qd = this.getDomain(qx, qy);
+        var domain = [Math.max(pd[0], qd[0]), Math.min(pd[1], qd[1])];
+
+        var pr = this.getRange(px, py);
+        var qr = this.getRange(qx, qy);
+        var range = [Math.max(pr[0], qr[0]), Math.min(pr[1], qr[1])];
+
+        for (var i = domain[0]; i <= domain[1]; i++) {
+            for (var j = Math.min(py, qy) + 1; j < Math.max(py, qy); j++) {
+                var k = i + this.width * j;
+                if (!(i < 0 || i > this.width - 1 || j < 0 || j > this.height - 1 || empty[k])) {
+                    break;
+                }
+            }
+            if (j == py || j == qy) {
+                paths.push([[px, py], [i, py], [i, qy], [qx, qy]]);
+            }
+        }
+
+        for (var j = range[0]; j <= range[1]; j++) {
+            for (var i = Math.min(px, qx) + 1; i < Math.max(px, qx); i++) {
+                var k = i + this.width * j;
+                if (!(i < 0 || i > this.width - 1 || j < 0 || j > this.height - 1 || empty[k])) {
+                    break;
+                }
+            }
+            if (i == px || i == qx) {
+                paths.push([[px, py], [px, j], [qx, j], [qx, qy]]);
+            }
+        }
+    }
+
+    return paths;
+}
