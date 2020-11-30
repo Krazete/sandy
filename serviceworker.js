df0e56efbaab94ca4889377a7e4b631756ee5c25
Ted Michael Celis <krazete@gmail.com>
2020-11-29 21:32:31 -0800

    replaced serviceworker
    my attempted version didn't work

--- serviceworker.js
+++ serviceworker.js
@@ -1,90 +1,59 @@
-var CACHE = "pwacache";
+// This is the "Offline copy of pages" service worker
 
-var everything = [
-    "/",
-    "/index.html",
-    "/index.css",
-    "/index.js",
-    "/modes.js",
-    "/x.js",
-    "/img/bg.png",
-    "/img/banner.png",
-    "/img/mic.png",
-    "/img/start.png",
-    "/img/3.png",
-    "/img/2.png",
-    "/img/1.png",
-    "/img/0.png",
-    "/img/record.png",
-    "/img/time.png",
-    "/img/selected.png",
-    "/img/shuffle.png",
-    "/img/result.png",
-    "/img/victory.png",
-    "/img/record.png",
-    "/img/again.png",
-    "/default/0.png",
-    "/default/1.png",
-    "/default/2.png",
-    "/default/3.png",
-    "/default/4.png",
-    "/default/5.png",
-    "/default/6.png",
-    "/default/7.png",
-    "/default/8.png",
-    "/default/9.png",
-    "/default/10.png",
-    "/default/11.png",
-    "/default/12.png",
-    "/default/13.png",
-    "/default/14.png",
-    "/default/15.png",
-    "/default/16.png",
-    "/default/17.png",
-    "/default/18.png",
-    "/vtuber/0.png",
-    "/vtuber/1.png",
-    "/vtuber/2.png",
-    "/vtuber/3.png",
-    "/vtuber/4.png",
-    "/vtuber/5.png",
-    "/vtuber/6.png",
-    "/vtuber/7.png",
-    "/vtuber/8.png",
-    "/vtuber/9.png",
-    "/vtuber/10.png",
-    "/vtuber/11.png",
-    "/vtuber/12.png",
-    "/vtuber/13.png",
-    "/vtuber/14.png",
-    "/vtuber/15.png",
-    "/vtuber/16.png",
-    "/vtuber/17.png",
-    "/vtuber/18.png",
-    "/vtuber/19.png",
-    "/vtuber/20.png"
-];
+const CACHE = "pwabuilder-offline";
 
+const offlineFallbackPage = "index.html";
+
+// Install stage sets up the index page (home page) in the cache and opens a new cache
 self.addEventListener("install", function (event) {
-    event.waitUntil(
-        caches.open(CACHE).then(
-            function (cache) {
-                console.log("Opened cache");
-                return cache.addAll(everything);
-            }
-        )
-    );
+  console.log("[PWA Builder] Install Event processing");
+
+  event.waitUntil(
+    caches.open(CACHE).then(function (cache) {
+      console.log("[PWA Builder] Cached offline page during install");
+      return cache.add(offlineFallbackPage);
+    })
+  );
 });
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request).then(
-            function(response) {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
-            }
-        )
-    );
+// If any fetch fails, it will look for the request in the cache and serve it from there first
+self.addEventListener("fetch", function (event) {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(function (response) {
+        console.log("[PWA Builder] add page to offline cache: " + response.url);
+
+        // If request was success, add or update it in the cache
+        event.waitUntil(updateCache(event.request, response.clone()));
+
+        return response;
+      })
+      .catch(function (error) {
+        console.log("[PWA Builder] Network request Failed. Serving content from cache: " + error);
+        return fromCache(event.request);
+      })
+  );
 });
+
+function fromCache(request) {
+  // Check to see if you have it in the cache
+  // Return response
+  // If not in the cache, then return error page
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      if (!matching || matching.status === 404) {
+        return Promise.reject("no-match");
+      }
+
+      return matching;
+    });
+  });
+}
+
+function updateCache(request, response) {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.put(request, response);
+  });
+}
